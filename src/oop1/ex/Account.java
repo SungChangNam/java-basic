66bba0bc8454d395414903fd70c12b383d5aa39f
Sung_cn_2 <112399821+SungChangNam@users.noreply.github.com>
2024-04-01 23:17:53 +0900

    절차지향 프로그램밍 이론 및 객체지향 이론 절자치향 프로그래밍을 객체지향 프로그래밍으로 리펙토리

---
+++ src/oop1/ex/Account.java
@@ -0,0 +1,19 @@
+package oop1.ex;
+
+public class Account {
+    int balance; // 작액
+
+    void dposit (int amount){
+        balance += amount;
+    }
+
+    void withdraw(int amount){
+        if (balance >=amount){
+            balance -= amount;
+        } else {
+            System.out.println("잔액을 부족");
+        }
+
+
+    }
+}
