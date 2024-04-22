62c0a8ca0ac92d84d879cc8399e8b51896cf5e7f
Sung_cn_2 <112399821+SungChangNam@users.noreply.github.com>
2024-04-22 23:34:36 +0900

    staticMethod

---
+++ src/static2/ex/CarMain.java
@@ -0,0 +1,13 @@
+package static2.ex;
+
+public class CarMain {
+    public static void main(String[] args) {
+        Car ca1 = new Car("K3");
+        Car ca2 = new Car("G80");
+
+
+        Car ca3 = new Car("Model Y");
+
+        Car.showTotalCars();
+    }
+}
---
+++ src/static2/ex/MathArrayUtiliIsMain.java
@@ -0,0 +1,12 @@
+package static2.ex;
+
+public class MathArrayUtiliIsMain {
+    public static void main(String[] args) {
+        int[] values = {1, 2, 3, 4,5};
+        System.out.println("sum= "+MathArrayUtils.sum(values));
+        System.out.println("avg= "+MathArrayUtils.avg(values));
+        System.out.println("min= "+MathArrayUtils.min(values));
+        System.out.println("max= "+MathArrayUtils.max(values));
+
+    }
+}
